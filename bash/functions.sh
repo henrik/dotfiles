f1504d219a721a62593fd082a62276b863233253
Henrik N <henrik@nyh.se>
2010-03-23 10:35:00 +0100

    Add pwf (print working file) bash function.

--- bash/functions.sh
+++ bash/functions.sh
@@ -1,6 +1,17 @@
-# henrik@Nyx ~$ cdgem activerecord
-# henrik@Nyx /Library/Ruby/Gems/1.8/gems/activerecord-3.0.0.beta$
-
+# cd gem
+#
+#     henrik@Nyx ~$ cdgem activerecord
+#     henrik@Nyx /Library/Ruby/Gems/1.8/gems/activerecord-3.0.0.beta$
+#
 function cdgem {
   cd /Library/Ruby/Gems/1.8/gems/; cd `ls|grep $1|sort|tail -1`
+}
+
+# print working file
+#
+#     henrik@Henrik ~/.dotfiles[master]$ pwf ackrc 
+#     /Users/henrik/.dotfiles/ackrc
+#
+function pwf {
+  echo "$PWD/$1"
 }
