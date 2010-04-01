f00ea17911cdfb2e939d592d74d132f42941b56e
Henrik N <henrik@nyh.se>
2010-04-01 23:58:04 +0200

    Bash: up arrow autocompletes from history, tab cycles commands instead of listing.
    From http://stackoverflow.com/questions/3746/whats-in-your-bashrc/172685#172685

---
+++ bash/bindings.sh
@@ -0,0 +1,6 @@
+# Autocomplete with up arrow.
+bind '"\e[A":history-search-backward'
+bind '"\e[B":history-search-forward'
+
+# Tab cycles through commands instead of listing.
+bind '"\t":menu-complete'
--- bash_profile
+++ bash_profile
@@ -3,3 +3,4 @@ source ~/.bash/env.sh
 source ~/.bash/prompt.sh
 source ~/.bash/aliases.sh
 source ~/.bash/functions.sh
+source ~/.bash/bindings.sh
