6595b1d1b1841cf153abf5c70e7ed6d45720f3ae
Henrik N <henrik@nyh.se>
2010-04-07 09:51:08 +0200

    bash: remove tab cycling, got annoying.

--- bash/bindings.sh
+++ bash/bindings.sh
@@ -1,6 +1,3 @@
 # Autocomplete with up arrow.
 bind '"\e[A":history-search-backward'
 bind '"\e[B":history-search-forward'
-
-# Tab cycles through commands instead of listing.
-bind '"\t":menu-complete'
