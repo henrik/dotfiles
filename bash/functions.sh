1432c97b1e3b85bd73b64a338eec2446cafdd5e9
Henrik N <henrik@nyh.se>
2010-05-11 22:50:21 +0200

    Add "xc" command to open .xcodeproj file in working directory.

--- bash/functions.sh
+++ bash/functions.sh
@@ -7,7 +7,7 @@ function cdgem {
   cd /Library/Ruby/Gems/1.8/gems/; cd `ls|grep $1|sort|tail -1`
 }
 
-# print working file
+# Print working file.
 #
 #     henrik@Henrik ~/.dotfiles[master]$ pwf ackrc 
 #     /Users/henrik/.dotfiles/ackrc
@@ -16,11 +16,21 @@ function pwf {
   echo "$PWD/$1"
 }
 
-# create directory and cd to it.
+# Create directory and cd to it.
 #
 #     henrik@Nyx /tmp$ mkcd foo/bar/baz
 #     henrik@Nyx /tmp/foo/bar/baz$
 #
 function mkcd {
   mkdir -p "$1" && cd "$1"
+}
+
+# Open the .xcodeproj file that is in the working directory (typically in Xcode).
+#
+#     henrik@Nyx ~/Code/iPhone/MyApp$ xc
+#     # equivalent to:
+#     open ~/Code/iPhone/MyApp/MyApp.xcodeproj
+#
+function xc {
+  open `ls | grep .xcodeproj`
 }
