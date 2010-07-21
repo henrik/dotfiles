d507fe8a8b002bb02a31a1e9560600a37589e76f
Henrik N <henrik@nyh.se>
2010-07-21 17:40:10 +0200

    Prefix bash prompt with current time.

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,11 +1,11 @@
 # http://blog.bitfluent.com/post/27983389/git-utilities-you-cant-live-without
 # http://superuser.com/questions/31744/how-to-get-git-completion-bash-to-work-on-mac-os-x
 
-# henrik@Nyx ~/.dotfiles[master*]$ 
+# 17:39 henrik@Nyx ~/.dotfiles[master*]$ 
 
 # MacPorts git bash completion.
 # Required for the prompt. Also provides autocompletion of git commands and branch names.
 . ~/.dotfiles/bash/lib/git-completion.bash
 
 export GIT_PS1_SHOWDIRTYSTATE=1
-export PS1='\[\e[1;33m\]\u@\h \[\e[0;33m\]\w\[\e[0m\]\[\e[32m\]$(__git_ps1 "[%s]")\[\e[0m\]$ '
+export PS1='\t \[\e[1;33m\]\u@\h \[\e[0;33m\]\w\[\e[0m\]\[\e[32m\]$(__git_ps1 "[%s]")\[\e[0m\]$ '
