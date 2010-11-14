29f253ec44f1be9302a039572de7f45a10fdf0fb
Henrik N <henrik@nyh.se>
2010-11-14 10:58:34 +0100

    "text" alias to open PlainText (http://www.hogbaysoftware.com/products/plaintext) files.
    Remove "todo" as those files are now a subset of PlainText.

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -3,8 +3,8 @@ alias a="cd ~/Sites/auktion"
 alias i="cd ~/Code/iPhone/AK"
 alias o="open ."  # OS X, open in Finder
 
-# TODO dir
-alias todo='mate ~/Dropbox/TODO'
+# http://www.hogbaysoftware.com/products/plaintext
+alias text='mate ~/Dropbox/PlainText'
 
 # Management
 alias dots="mate ~/.dotfiles"
