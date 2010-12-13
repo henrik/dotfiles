2dccc3d4edae6168302d48dd99e0467fe4e9b22d
Henrik N <henrik@nyh.se>
2010-12-13 20:31:22 +0100

    Use tilde instead of explicit home dir in bash PATH.

--- bash/path.sh
+++ bash/path.sh
@@ -3,12 +3,12 @@ export PATH=/usr/local/bin:/usr/local/sbin:$PATH
 export MANPATH=/opt/local/share/man:/usr/local/man:$MANPATH
 
 # Homebrew
-export PATH=/Users/henrik/.homebrew/bin:/Users/henrik/.homebrew/sbin:$PATH
-export MANPATH=/Users/henrik/.homebrew/man:$MANPATH
+export PATH=~/.homebrew/bin:~/.homebrew/sbin:$PATH
+export MANPATH=~/.homebrew/man:$MANPATH
 
 # Get the Homebrew MacVim version of vim on the command-line too.
 # Brew doesn't have a plain Vim recipe and the OS X system Vim doesn't come with Ruby support.
-export PATH=/Users/henrik/.homebrew/Cellar/macvim/HEAD/MacVim.app/Contents/MacOS:$PATH
+export PATH=~/.homebrew/Cellar/macvim/HEAD/MacVim.app/Contents/MacOS:$PATH
 
 # Ubuntu Ruby gem binaries
 export PATH=/var/lib/gems/1.8/bin:$PATH
@@ -17,7 +17,7 @@ export PATH=/var/lib/gems/1.8/bin:$PATH
 export PATH=/usr/local/mysql/bin:$PATH
 
 # Jekyll (use dev version, not gem)
-export PATH=/Users/henrik/Code/jekyll/bin:$PATH
+export PATH=~/Code/jekyll/bin:$PATH
 
 # RVM
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" 
