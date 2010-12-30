48e01cfd43d2cf389f54d91efa1000b25f92d8ef
Henrik N <henrik@nyh.se>
2010-12-30 23:28:42 +0100

    Remove migg bash function since Rails.vim has :Rgenerate migration.

--- bash/functions.sh
+++ bash/functions.sh
@@ -56,12 +56,3 @@ function sshkey {
 function xc {
   open `ls | grep .xcodeproj`
 }
-
-
-# Generate a Ruby on Rails migration, then open the generated file.
-#
-#     henrik@Nyx /myproject$ migg add_fooed_at_to_bars fooed_at:datetime
-
-function migg {
-  script/generate migration $@ | ruby -e 'x = ARGF.read; puts x; path = x[/create\s+(.+)/, 1]; system("open", path)'
-}
