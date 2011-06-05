ec55fecd5a5be4b4899cacc0896262517dce160b
Henrik N <henrik@nyh.se>
2011-06-05 18:30:43 +0200

    Use autojump in shell.

--- bash/path.sh
+++ bash/path.sh
@@ -18,5 +18,11 @@ export PATH=~/Code/jekyll/bin:$PATH
 # RVM
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" 
 
+# brew install autojump
+# https://github.com/joelthelion/autojump
+if [ -f `brew --prefix`/etc/autojump ]; then
+  . `brew --prefix`/etc/autojump
+fi
+
 # To get the GUI-less Vim from MacVim:
 # ln -s ~/.homebrew/bin/mvim ~/.homebrew/bin/vim
