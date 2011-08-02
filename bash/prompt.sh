8ade6671233f72f9e74298adb12a420dd38cf058
Henrik N <henrik@nyh.se>
2011-08-02 22:14:04 +0200

    Clean up prompt. Add RVM version.
    Based mostly on http://tammersaleh.com/posts/a-better-rvm-bash-prompt.

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -1,5 +1,7 @@
 # http://blog.bitfluent.com/post/27983389/git-utilities-you-cant-live-without
 # http://superuser.com/questions/31744/how-to-get-git-completion-bash-to-work-on-mac-os-x
+# http://tammersaleh.com/posts/a-better-rvm-bash-prompt
+# https://rvm.beginrescueend.com/workflow/prompt/
 
 # 17:39 henrik@Nyx ~/.dotfiles[master*]$ 
 
@@ -7,5 +9,43 @@
 # Required for the prompt. Also provides autocompletion of git commands and branch names.
 . ~/.dotfiles/bash/lib/git-completion.bash
 
-export GIT_PS1_SHOWDIRTYSTATE=1
-export PS1='\t \[\e[1;33m\]\u@\h \[\e[0;33m\]\w\[\e[0m\]\[\e[32m\]$(__git_ps1 "[%s]")\[\e[0m\]$ '
+function __rvm_prompt {
+  rvm-prompt i v g s
+}
+
+function __git_prompt {
+  GIT_PS1_SHOWDIRTYSTATE=1
+  __git_ps1 " %s" | sed 's/ \*$/*/'
+}
+
+bash_prompt() {
+
+  # regular colors
+  local K="\[\033[0;30m\]"    # black
+  local R="\[\033[0;31m\]"    # red
+  local G="\[\033[0;32m\]"    # green
+  local Y="\[\033[0;33m\]"    # yellow
+  local B="\[\033[0;34m\]"    # blue
+  local M="\[\033[0;35m\]"    # magenta
+  local C="\[\033[0;36m\]"    # cyan
+  local W="\[\033[0;37m\]"    # white
+
+  # emphasized (bolded) colors
+  local BK="\[\033[1;30m\]"
+  local BR="\[\033[1;31m\]"
+  local BG="\[\033[1;32m\]"
+  local BY="\[\033[1;33m\]"
+  local BB="\[\033[1;34m\]"
+  local BM="\[\033[1;35m\]"
+  local BC="\[\033[1;36m\]"
+  local BW="\[\033[1;37m\]"
+
+  # reset
+  local RESET="\[\033[0;37m\]"
+
+  PS1="\t $BY\u@\h $Y\w$G[\$(__rvm_prompt)$G\$(__git_prompt)]$RESET$ "
+
+}
+
+bash_prompt
+unset bash_prompt
