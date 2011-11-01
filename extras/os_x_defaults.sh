7353772fd623a019cc6bec5e7536cb7f1d819e12
Henrik N <henrik@nyh.se>
2011-11-01 09:46:07 +0100

    More defaults write OS X config.

--- extras/os_x_defaults.sh
+++ extras/os_x_defaults.sh
@@ -20,6 +20,10 @@ defaults write com.apple.frameworks.diskimages skip-verify -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
+
 # Display full POSIX path as Finder window title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
