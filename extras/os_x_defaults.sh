e0d90262fc5239e7acde02b94f404c16a7da2a22
Henrik N <henrik@nyh.se>
2012-04-01 10:17:20 +0200

    Tweak OS X defaults.

--- extras/os_x_defaults.sh
+++ extras/os_x_defaults.sh
@@ -1,38 +1,68 @@
-# Based on https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+# Based on https://github.com/mathiasbynens/dotfiles/blob/master/.osx.
 
-# Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)
+echo "Enable full keyboard access for all controls (e.g. enable Tab in modal dialogs)."
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-# Disable menu bar transparency
+echo "Disable menu bar transparency."
 defaults write NSGlobalDomain AppleEnableMenuBarTransparency -bool false
 
-# Expand save panel by default
+echo "Expand save panel by default."
 defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
 
-# Disable the “Are you sure you want to open this application?” dialog
+echo "Disable the 'Are you sure you want to open this application?' dialog."
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-# Disable window animations
+echo "Disable window animations."
 defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false
 
-# Disable disk image verification
+echo "Set a blazingly fast keyboard repeat rate."
+defaults write NSGlobalDomain KeyRepeat -int 0.02
+
+echo "Set a shorter Delay until key repeat."
+defaults write NSGlobalDomain InitialKeyRepeat -int 12
+
+echo "Increase window resize speed for Cocoa applications."
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+echo "Show item info below desktop icons."
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:showItemInfo true" ~/Library/Preferences/com.apple.finder.plist
+
+echo "Enable snap-to-grid for desktop icons."
+/usr/libexec/PlistBuddy -c "Set :DesktopViewSettings:IconViewSettings:arrangeBy grid" ~/Library/Preferences/com.apple.finder.plist
+
+echo "Disable disk image verification."
 defaults write com.apple.frameworks.diskimages skip-verify -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
-# Automatically open a new Finder window when a volume is mounted
+echo "Automatically open a new Finder window when a volume is mounted."
 defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool true
 defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool true
 
-# Display full POSIX path as Finder window title
+echo "Display full POSIX path as Finder window title."
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-# Show the ~/Library folder
+echo "Show Path bar in Finder."
+defaults write com.apple.finder ShowPathbar -bool true
+
+echo "Show Status bar in Finder."
+defaults write com.apple.finder ShowStatusBar -bool true
+
+echo "Use current directory as default search scope in Finder."
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+echo "Show the ~/Library folder."
 chflags nohidden ~/Library
+
+echo "Disable the Ping sidebar in iTunes."
+defaults write com.apple.iTunes disablePingSidebar -bool true
+
+echo "Disable all the other Ping stuff in iTunes."
+defaults write com.apple.iTunes disablePing -bool true
 
 echo "Changed defaults. Restarting apps…"
 
-# Kill affected applications
+echo "Kill affected applications"
 for app in Finder; do killall "$app"; done
 
 echo "Restarted apps. All done."
