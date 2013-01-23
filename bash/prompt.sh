acf7ffe4d8224de7fd0ee3e100c135e6aa5e609a
Henrik N <henrik@nyh.se>
2013-01-23 19:39:17 +0100

    Bash prompt: basedir, not full path.
    Saves space, especially now that I'm experimenting with a larger terminal font.
    
    When I use autojump, it will show the pwd. Otherwise I can run pwd manually.

--- bash/prompt.sh
+++ bash/prompt.sh
@@ -3,7 +3,7 @@
 # http://tammersaleh.com/posts/a-better-rvm-bash-prompt
 # https://rvm.beginrescueend.com/workflow/prompt/
 
-# 17:39:15 henrik@Nyx ~/project[ree-1.8.7@project master*]$ 
+# 17:39:15 henrik@Nyx project_dir[ree-1.8.7@project master*]$ 
 
 function __rvm_prompt {
   if hash rvm-prompt 2>&- ; then
@@ -50,7 +50,7 @@ bash_prompt() {
   # reset
   local RESET="\[\033[0;37m\]"
 
-  PS1="\t $BY\$(__name_and_server)$Y\w$G[\$(__rvm_prompt)$G\$(__git_prompt)]$RESET$ "
+  PS1="\t $BY\$(__name_and_server)$Y\W$G[\$(__rvm_prompt)$G\$(__git_prompt)]$RESET$ "
 
 }
 
