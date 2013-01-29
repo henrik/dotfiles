76e1c20c4063721e852a113177e3ff99bbe92f69
Henrik N <henrik@nyh.se>
2013-01-29 08:17:19 +0100

    Set LANG to fix Pow non-ASCII error handling.

--- bash/env.sh
+++ bash/env.sh
@@ -4,3 +4,6 @@ export OCTOPRESS_EDITOR='vim +'  # Go to last line.
 export RBXOPT=-X19
 # No duplicates in history.
 export HISTCONTROL=ignoredups
+# Pow shouldn't hide errors in non-ASCII apps:
+# https://github.com/37signals/pow/issues/268
+export LANG=sv_SE.UTF-8
