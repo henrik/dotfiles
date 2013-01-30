c1aae51fdac44f08c0fd389a1e9b4cfadf919796
Henrik N <henrik@nyh.se>
2013-01-30 22:45:16 +0100

    note_to_self.rb binary.
    Read more: https://gist.github.com/4677108

---
+++ bin/note_to_self.rb
@@ -0,0 +1,55 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# Command-line tool to mail notes to self with Gmail.
+#
+# Install gems if you haven't:
+#   gem install gmail active_support
+#
+# Configure:
+
+TO = "henrik@nyh.se"
+LOGIN = "robot@nyh.se"
+PW = File.read(File.expand_path("~/.gmailpw")).chop
+
+# Intended to be used with an Alfred.app extension like:
+#
+#   source ~/.rvm/scripts/rvm
+#   ~/.bin/note_to_self.rb {query}
+
+# https://github.com/nu7hatch/gmail
+require "gmail"
+
+# To fix Alfred.app encoding :(
+require "active_support/all"
+
+def init
+  message = ARGV.first
+
+  # Alfred passes decomposed characters which cause the mail to silently fail.
+  message = message.mb_chars.normalize
+
+  note_to_self(message)
+end
+
+def note_to_self(message)
+  send_mail(LOGIN, PW, TO, message, message)
+  puts "Sent: #{message}"
+rescue StandardError => e
+  puts "Error sending: #{e.class.name}: #{e.message}"
+end
+
+def send_mail(login, pw, _to, _subject, _body)
+  Gmail.connect!(login, pw) do |gmail|
+    gmail.deliver do
+      to _to
+      subject _subject
+      text_part do
+        content_type 'text/plain; charset=UTF-8'
+        body _body
+      end
+    end
+  end
+end
+
+init
