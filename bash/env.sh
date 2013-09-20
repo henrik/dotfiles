22cbc71a60ccbc7190f3e13d8c76e15412149e4a
Henrik Nyh <henrik@nyh.se>
2013-09-20 11:55:42 +0200

    Change LANG to the Barsoom standard.

--- bash/env.sh
+++ bash/env.sh
@@ -11,6 +11,6 @@ export TERM='xterm-256color'
 # Pow shouldn't hide errors in non-ASCII apps:
 # https://github.com/37signals/pow/issues/268
 # Also fixes UTF-8 display in e.g. git log.
-export LANG=en_US.UTF-8
+export LANG=en_GB.UTF-8
 export LC_ALL=$LANG
 export LC_CTYPE=$LANG
