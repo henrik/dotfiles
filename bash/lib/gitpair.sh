77dbc5f01b3ead957aa834fcd9142dc30406fb25
Henrik Nyh <henrik@nyh.se>
2013-10-02 09:24:49 +0200

    Unbork unpair

--- bash/lib/gitpair.sh
+++ bash/lib/gitpair.sh
@@ -2,7 +2,7 @@
 # Also see prompt.sh.
 
 alias pair='echo "Committing as: `git config user.name` <`git config user.email`>"'
-alias unpair="git config remove-section user; echo Unpaired.; pair"
+alias unpair="git config --remove-section user; echo Unpaired.; pair"
 
 alias pairj="git config user.pair 'HN+JK' && git config user.name 'Henrik Nyh and Joakim Kolsjö' && git config user.email 'all+henrik+jocke@barsoom.se'; pair"
 alias pairk="git config user.pair 'HN+KP' && git config user.name 'Henrik Nyh and Kim Persson' && git config user.email 'all+henrik+kim@barsoom.se'; pair"
