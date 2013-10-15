530f886f15e51733166af9502c28acf796a84098
Henrik Nyh <henrik@nyh.se>
2013-10-15 11:04:25 +0200

    Another week, another pair.

--- bash/lib/gitpair.sh
+++ bash/lib/gitpair.sh
@@ -6,3 +6,4 @@ alias unpair="git config --remove-section user 2> /dev/null; echo Unpaired.; pai
 
 alias pairj="git config user.pair 'HN+JK' && git config user.name 'Henrik Nyh and Joakim Kolsjö' && git config user.email 'all+henrik+jocke@barsoom.se'; pair"
 alias pairk="git config user.pair 'HN+KP' && git config user.name 'Henrik Nyh and Kim Persson' && git config user.email 'all+henrik+kim@barsoom.se'; pair"
+alias pairt="git config user.pair 'HN+TS' && git config user.name 'Henrik Nyh and Tomas Skogberg' && git config user.email 'all+henrik+tomas@barsoom.se'; pair"
