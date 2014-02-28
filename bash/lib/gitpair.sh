59506f914fbc6e739b3279cc465519a299d3e7e5
Henrik Nyh <henrik@nyh.se>
2014-02-28 21:27:16 +0100

    gitpair.sh: alignment

--- bash/lib/gitpair.sh
+++ bash/lib/gitpair.sh
@@ -12,7 +12,7 @@ alias pairamend='git commit --amend -C HEAD --author="`getpair`"'
 setpair() { git config user.pair "$1" && git config user.name "$2" && git config user.email "$3"; pair; }
 
 # Add one of these for every pair.
-alias pairj='setpair "HN+JK" "Henrik Nyh and Joakim Kolsjö" "all+henrik+jocke@barsoom.se"'
-alias pairk='setpair "HN+KP" "Henrik Nyh and Kim Persson" "all+henrik+kim@barsoom.se"'
-alias pairt='setpair "HN+TS" "Henrik Nyh and Tomas Skogberg" "all+henrik+tomas@barsoom.se"'
-alias pairv='setpair "HN+VA" "Henrik Nyh and Victor Arias" "all+henrik+victor@barsoom.se"'
+alias pairj='setpair  "HN+JK"  "Henrik Nyh and Joakim Kolsjö"   "all+henrik+jocke@barsoom.se"'
+alias pairk='setpair  "HN+KP"  "Henrik Nyh and Kim Persson"     "all+henrik+kim@barsoom.se"'
+alias pairt='setpair  "HN+TS"  "Henrik Nyh and Tomas Skogberg"  "all+henrik+tomas@barsoom.se"'
+alias pairv='setpair  "HN+VA"  "Henrik Nyh and Victor Arias"    "all+henrik+victor@barsoom.se"'
