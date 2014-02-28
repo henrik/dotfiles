dd4b63b6f126d651b0e038a2c16fc03857dbad94
Henrik Nyh <henrik@nyh.se>
2014-02-28 21:08:56 +0100

    gitpair: add 'pairamend' command to fix pair after committing.

--- bash/lib/gitpair.sh
+++ bash/lib/gitpair.sh
@@ -1,9 +1,13 @@
-# Simple git pair. Like "hitch" but simpler/less buggy for me. WiP.
+# Simple git pair. Like "hitch" but simpler/less buggy for me.
 # Also see prompt.sh.
 
 alias pair='echo "Committing as: `git config user.name` <`git config user.email`>"'
 alias unpair="git config --remove-section user 2> /dev/null; echo Unpaired.; pair"
 
+# Amend the last commit with the current pair (when you forget to set the pair until after committing.)
+alias pairamend='git commit --amend -C HEAD --author="`git config user.name` <`git config user.email`>"'
+
+# Add one of these for every pair.
 alias pairj="git config user.pair 'HN+JK' && git config user.name 'Henrik Nyh and Joakim Kolsjö' && git config user.email 'all+henrik+jocke@barsoom.se'; pair"
 alias pairk="git config user.pair 'HN+KP' && git config user.name 'Henrik Nyh and Kim Persson' && git config user.email 'all+henrik+kim@barsoom.se'; pair"
 alias pairt="git config user.pair 'HN+TS' && git config user.name 'Henrik Nyh and Tomas Skogberg' && git config user.email 'all+henrik+tomas@barsoom.se'; pair"
