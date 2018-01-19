cb407f4fd34c85aff825d04f7862dca5c405908a
Henrik Nyh <henrik@nyh.se>
2018-01-19 11:59:30 +0000

    Remove explicit setting of locale
    Caused issues with our new work devbox and I'm not sure it's needed anymore.

--- bash/env.sh
+++ bash/env.sh
@@ -7,10 +7,3 @@ export HISTCONTROL=ignoredups
 
 # Colors in Vim.
 export TERM='xterm-256color'
-
-# Pow shouldn't hide errors in non-ASCII apps:
-# https://github.com/37signals/pow/issues/268
-# Also fixes UTF-8 display in e.g. git log.
-export LANG=en_GB.UTF-8
-export LC_ALL=$LANG
-export LC_CTYPE=$LANG
