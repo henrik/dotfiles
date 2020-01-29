03b79723785ba6692108d9e5b45c25efabbaa0c2
Henrik Nyh <henrik@nyh.se>
2020-01-29 12:56:08 +0000

    Update gitpairs

--- bash/lib/gitpair.sh
+++ bash/lib/gitpair.sh
@@ -13,11 +13,12 @@ setpair() { git config user.pair "$1" && git config user.email "$2" && git confi
 
 # Add one of these for every pair. Each pair should be in alphabetical order by first name.
 alias paira='setpair  AR+HN  all+albert+henrik@barsoom.se  "Albert Ramstedt and Henrik Nyh"'
+alias pairap='setpair  AR+HN+PW  all+albert+henrik+peter@barsoom.se   "Albert Ramstedt and Henrik Nyh and Peter Wall"'
 alias pairj='setpair  HN+JK  all+henrik+jocke@barsoom.se   "Henrik Nyh and Joakim Kolsjö"'
 alias pairk='setpair  HN+KE  all+henrik+kenneth@barsoom.se "Henrik Nyh and Kenneth Eriksson"'
+alias pairp='setpair  HN+PW  all+henrik+peter@barsoom.se   "Henrik Nyh and Peter Wall"'
+alias pairpt='setpair  HN+PW+TS  all+henrik+peter+tomas@barsoom.se   "Henrik Nyh and Peter Wall and Tomas Skogberg"'
 alias pairt='setpair  HN+TS  all+henrik+tomas@barsoom.se   "Henrik Nyh and Tomas Skogberg"'
-alias pairv='setpair  HN+VA  all+henrik+victor@barsoom.se  "Henrik Nyh and Victor Arias"'
 alias pairl='setpair  HN+LF  all+henrik+lennart@barsoom.se  "Henrik Nyh and Lennart Fridén"'
-alias pairf='setpair  FH+HN  all+fabian+henrik@barsoom.se  "Fabian Hoffmann and Henrik Nyh"'
 alias pairc='setpair  CE+HN  all+calle+henrik@barsoom.se  "Calle Erlandsson and Henrik Nyh"'
 alias pairmob='setpair MOB devs@auctionet.com "Mob"'
