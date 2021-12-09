ce7cc4a4309154e9150843c87a36a560a71abc79
Henrik Nyh <henrik@nyh.se>
2021-12-09 17:16:28 +0000

    Set HOMEBREW_NO_AUTO_UPDATE=1

--- bash/env.sh
+++ bash/env.sh
@@ -5,6 +5,9 @@ export RBXOPT=-X19
 # No duplicates in history.
 export HISTCONTROL=ignoredups
 
+# Don't update the whole world when "brew install"-ing (https://github.com/Homebrew/brew/issues/1670).
+export HOMEBREW_NO_AUTO_UPDATE=1
+
 # Colors in Vim.
 export TERM='xterm-256color'
 
